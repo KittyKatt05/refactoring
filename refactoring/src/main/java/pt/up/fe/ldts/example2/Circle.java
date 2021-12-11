c062958c47a610f50950a9303ace42959b7f9cb1
Catarina <1201338@isep.ipp.pt>
2021-12-11 21:40:33 +0000

    Commit do exemplo 2

---
+++ refactoring/src/main/java/pt/up/fe/ldts/example2/Circle.java
@@ -0,0 +1,35 @@
+package pt.up.fe.ldts.example2;
+
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(int x, int y, double radius){
+        super(x,y);
+        this.radius = radius;
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea(){
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getPerimeter(){
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public void draw(GraphicFramework graphics) {
+        graphics.drawCircle(x, y, radius);
+    }
+}
