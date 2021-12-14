b566209c8feaf15d4ea6fce58dfd433fb6244ba4
Catarina <1201338@isep.ipp.pt>
2021-12-14 23:20:00 +0000

    Commit do exemplo 2 correção

--- refactoring/src/main/java/pt/up/fe/ldts/example2/Shape.java
+++ refactoring/src/main/java/pt/up/fe/ldts/example2/Shape.java
@@ -1,15 +1,11 @@
 package pt.up.fe.ldts.example2;
 
 public abstract class Shape {
-    enum TYPE {RECTANGLE, CIRCLE}
-
-    private TYPE type;
     protected double x;
     protected double y;
 
 
     public Shape(double x, double y) {
-        this.type = TYPE.RECTANGLE;
         this.x = x;
         this.y = y;
     }
